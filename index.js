b7db483341df4a08ba6899422c4c4e6b9d9bf8da
Maryel M <78126163+maryel-m@users.noreply.github.com>
2025-01-08 16:13:01 -0500

    Practice writing functions on console.

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 // document.getElementById("count-el").innerText = 5
 
-// let count = 5;
-// count = count + 1
-// console.log(count);
+let count = 0;
 
 function increment() {
-    console.log("The button was clicked")
+  count = count + 1;
+  console.log(count);
 }
+
+// function increment() {
+//    console.log("The button was clicked")
+// }
+
+///////// PRACTICE CODE BELOW /////////
+// let lap1 = 34;
+// let lap2 = 33;
+// let lap3 = 36;
+
+// function laptotal() {
+//   console.log(lap1 + lap2 + lap3);
+// }
+
+// laptotal();
+
+// let lapsCompleted = 0;
+
+// oldValue = oldValue +1
+// function incrementLaps() {
+//   lapsCompleted = lapsCompleted + 1;
+// }
+
+// Invoke 3x
+// incrementLaps();
+// incrementLaps();
+// incrementLaps();
+
+// console.log(lapsCompleted);
