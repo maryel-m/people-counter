1d46b6214b47f3ba7caebb8b69dc19159bf34c22
Maryel M <78126163+maryel-m@users.noreply.github.com>
2025-01-08 15:01:04 -0500

    Add onclick event, increment function.

---
+++ index.js
@@ -0,0 +1,9 @@
+// document.getElementById("count-el").innerText = 5
+
+// let count = 5;
+// count = count + 1
+// console.log(count);
+
+function increment() {
+    console.log("The button was clicked")
+}
